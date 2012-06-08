a63c8141a15887491b4d5b78e1d58de2b9f852b9
Josh Butts <josh@joshbutts.com>
2012-06-07 21:56:14 -0500

    initial commit

---
+++ bin/makephar.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: josh
+ * Date: 6/7/12
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/Pseudo/PdoException.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: josh
+ * Date: 6/7/12
+ * Time: 6:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
