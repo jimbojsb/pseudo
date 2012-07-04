8cc36276a710768cca37e8cab2b16d1ea68052a4
Josh Butts <josh@joshbutts.com>
2012-07-03 22:16:39 -0500

    add phar generator

--- bin/makephar.php
+++ bin/makephar.php
@@ -1,8 +1,15 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: josh
- * Date: 6/7/12
- * Time: 6:13 PM
- * To change this template use File | Settings | File Templates.
- */
+
+$phar = new Phar(__DIR__ . '/../pseudo.phar');
+$phar->buildFromDirectory(__DIR__ . '/../src');
+
+$runFile = file_get_contents(__DIR__ . '/../src/autoload.php');
+$runFile = str_replace("__DIR__", "'phar://' . __FILE__ ", $runFile);
+$runFile = str_replace('/../src', '', $runFile) . PHP_EOL;
+$runFile .= "Phar::mapPhar();" . PHP_EOL;
+$runFile .= "__HALT_COMPILER();" . PHP_EOL;
+$phar->setStub($runFile);
+
+//quick test to make sure it works
+require_once __DIR__ . '/../pseudo.phar';
+$p = new Pseudo\Pdo();
