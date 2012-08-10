930a341d8a16d9cf4314f875336914a7cb8e0936
Josh Butts <josh@joshbutts.com>
2012-08-10 14:58:42 -0500

    add phar support

--- bin/makephar.php
+++ bin/makephar.php
@@ -1,15 +1,13 @@
 <?php
-
 $phar = new Phar(__DIR__ . '/../pseudo.phar');
 $phar->buildFromDirectory(__DIR__ . '/../src');
 
-$runFile = file_get_contents(__DIR__ . '/../src/autoload.php');
+$runFile = implode(PHP_EOL, array_slice(file(__DIR__ . '/../src/autoload.php'), 2));
 $runFile = str_replace("__DIR__", "'phar://' . __FILE__ ", $runFile);
-$runFile = str_replace('/../src', '', $runFile) . PHP_EOL;
-$runFile .= "Phar::mapPhar();" . PHP_EOL;
-$runFile .= "__HALT_COMPILER();" . PHP_EOL;
-$phar->setStub($runFile);
+$runFile = str_replace('/../src', '', $runFile);
 
-//quick test to make sure it works
-require_once __DIR__ . '/../pseudo.phar';
-$p = new Pseudo\Pdo();
+$stub .= "<?php" . PHP_EOL;
+$stub .= "Phar::mapPhar();" . PHP_EOL;
+$stub .= $runFile . PHP_EOL;
+$stub .= "__HALT_COMPILER();" . PHP_EOL;
+$phar->setStub($stub);
---
+++ src/Pseudo/Recorder.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: josh
+ * Date: 6/28/12
+ * Time: 5:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
