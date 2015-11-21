4f6bfa9cd7090263dcd526d27cc3787c5c0be333
Nayana Hettiarachchi <nayana@corp-gems.com>
2015-11-22 05:39:09 +0700

    revert unintentional change to param default value

--- src/Pseudo/PdoStatement.php
+++ src/Pseudo/PdoStatement.php
@@ -41,7 +41,7 @@ class PdoStatement extends \PDOStatement
         }
     }
 
-    public function fetch($fetch_style = NULL, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0)
+    public function fetch($fetch_style = null, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0)
     {
         // scrolling cursors not implemented
         $row = $this->result->nextRow();
